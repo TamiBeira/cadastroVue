aa7c207cb54c56bf10c907d39de44e624a48c22f
Tamiris Beira <tamirisbeira@gmail.com>
2021-10-16 19:30:37 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,83 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <h3>Cadastro</h3>
+    <small id="deuErro" v-show="deuErro1">Nome inválido tente novamente!</small><br>
+    <input type="text" placeholder="nome" v-model="nomeField"/><br>
+    <small id="deuErro" v-show="deuErro2">Idade inválida tente novamente!</small><br>
+    <input type="number" placeholder="idade" v-model="idadeField"/><br>
+    <small id="deuErro" v-show="deuErro3">Email inválido tente novamente!</small><br>
+    <input type="text" placeholder="email" v-model="emailField"/><br>
+    <input @click="gravar()" type="submit" value="Cadastrar">    
+    
+    <hr>
+
+    <div v-for="cliente in clientes" :key="cliente.id" >
+          <Cliente :cliente="cliente"/>
+    </div>
+
   </div>
   <router-view/>
 </template>
 
+
+<script>
+import Cliente from './components/Cliente.vue';
+
+
+export default {
+  name: 'App',
+    data(){
+      return{
+        deuErro1: false,
+        deuErro2: false,
+        deuErro3: false,
+        nomeField:'',
+        idadeField:0,
+        emailField:'',
+
+        clientes: [
+          {id:1, nome:'Albert Einsten', idade:50, email:'alberteinsten@gmail.com'},
+          {id:2, nome:'Nikola Tesla', idade:45, email:'nikolatesla@gmail.com'},
+          {id:3, nome:'Grace Hopper', idade:40, email:'gracehopper@gmail.com'}
+        ]
+        }
+    },
+  components:{
+    Cliente
+  },
+  methods:{
+    gravar: function(){
+      if(this.nomeField === ''|| this.nomeField === ' ' || this.nomeField === undefined){
+        this.deuErro1 = true;
+      }else if(this.idadeField <= 0){
+        this.deuErro2 = true;
+      }else if(this.emailField === ''|| this.emailField === ' ' || this.emailField === undefined){
+        this.deuErro3 = true;
+      }else{
+        this.clientes.push({nome:this.nomeField, idade:this.idadeField, email:this.emailField, id:Date.data})
+        this.nomeField ='';
+        this.idadeField ='';
+        this.emailField ='';
+        this.deuErro1 = false;
+        this.deuErro2 = false;
+        this.deuErro3 = false;
+      }
+    }      
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #app{
+    width: 100%;
+  }
+  #app input{
+    padding: 10px;
+    width: 300px;
+  }
+  #deuErro{
+    color: red;
+  }
 </style>
+
+
---
+++ src/components/Cliente.vue
@@ -0,0 +1,51 @@
+<template>
+    <div :class="{'cliente': !isPremium, 'cliente-premium':isPremium}">
+        <h2>Cliente: {{cliente.nome}}</h2>
+        <p><strong>Idade:</strong> {{cliente.idade}}</p>
+         <!-- <p v-if="showIdade === true"><strong>Idade:</strong> {{cliente.idade}}</p> -->
+        <!-- <p v-else><strong>O usuário escondeu a idade</strong></p> -->
+        <p><strong>Email:</strong> {{cliente.email}} </p>
+        <button @click="mudarCor($event)">Mudar Cor</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            isPremium: false
+        }
+    },
+    props:{
+        cliente: Object
+    },
+    methods:{
+        mudarCor: function($event){
+            console.log($event);
+            this.isPremium = !this.isPremium;
+        }
+    }
+}
+</script>
+
+<style>
+    .cliente{
+        width: 450px;
+        display: flex;
+        flex-direction: column;
+        padding: 20px;
+        margin-top: 10px;
+        border: 1px solid gray;
+    }
+    .cliente-premium{
+        background-color: black;
+        color: goldenrod;
+
+        width: 450px;
+        display: flex;
+        flex-direction: column;
+        padding: 20px;
+        margin-top: 10px;
+        border: 1px solid gray;
+    }
+</style>
